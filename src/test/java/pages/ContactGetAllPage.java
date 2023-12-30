88c4f8a1eb0fc674aa579f38b0730f16316266ae
Behice <kinaspen@gmail.com>
2023-12-30 13:22:57 +0200

    behice.30.12.23

--- src/test/java/pages/ContactGetAllPage.java
+++ src/test/java/pages/ContactGetAllPage.java
@@ -1,17 +1,43 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class ContactGetAllPage {
 
-
-    public static WebElement ContactGetAll;
-
-    public void ContactGetAllPage() {
+    public ContactGetAllPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+    @FindBy(xpath = "//*[.='Contact']")
+    public WebElement contactLink;
+
+    @FindBy(id = "name")
+    public WebElement name;
+
+    @FindBy(id = "email")
+    public WebElement email;
+
+    @FindBy(id = "subject")
+    public WebElement subject;
+
+    @FindBy(id = "message")
+    public WebElement message;
+
+    @FindBy(xpath = "//*[.='Send Message']")
+    public WebElement sendMessageButton;
+
+    @FindBy(xpath = "//*[.='Required']")
+    public WebElement requiredTextMessage;
+
+    @FindBy(xpath = "//div[@role='alert']")
+    public WebElement contactCreatedAlertMessage;
+
+    @FindBy(xpath = "//div[@role='alert']")
+    public WebElement invalidEmailAlertMessage;
+
+
 
 
 
