10825b0d37f045fcb844fc79b1772809453334dc
fatmakaa <142013126+fatmakaa@users.noreply.github.com>
2024-01-04 22:05:29 +0300

    fatma

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -13,4 +13,26 @@ public class HomePage {
 
     @FindBy(xpath = "//a[@href='/login']")
     public WebElement loginSY;
+
+
+
+
+    //Fatma Karmis
+    @FindBy(xpath = "//a[@class='header_link ms-2']")
+    public WebElement loginFK;
+
+
+    @FindBy(id = "username")
+    public WebElement userNameFK;
+
+
+    @FindBy(id = "password")
+    public WebElement passwordFK;
+
+
+    @FindBy(xpath = "//button[@class='fw-semibold btn btn-primary']")
+    public WebElement loginButtonFK;
+
+
 }
+
